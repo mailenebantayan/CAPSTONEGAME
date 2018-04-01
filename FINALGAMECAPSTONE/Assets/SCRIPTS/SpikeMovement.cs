5382d2cba8fd0820c787e40c55e455cc1cc54f0f
mailenebantayan <mailenecbantayan@gmail.com>
2018-04-01 16:24:14 +0900

    saving progress

---
+++ FINALGAMECAPSTONE/Assets/SCRIPTS/SpikeMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeMovement : MonoBehaviour {
+
+	public float speed = .5f;
+	public GameObject character;
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		double distancey = transform.position.y + 2;
+		double distancex = transform.position.x + 2;
+
+		if (transform.position.x <= character.transform.position.x && distancex >= character.transform.position.x || distancey >= character.transform.position.y && transform.position.y <= character.transform.position.y) {
+			
+				transform.Translate (0, -speed * Time.deltaTime, 0);
+		
+		} else {
+				transform.Translate (0, speed * Time.deltaTime, 0);
+		
+		}
+
+
+
+
+	}
+}
